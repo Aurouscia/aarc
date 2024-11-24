b8ccf7de7ad7daa5d84c0e67ab8ceffce5a0dfef
Au <18971501210@189.cn>
2024-11-24 20:50:04 +0800

    环线头尾点标记改为与线路同色

--- AARC-Frontend/src/models/cvs/workers/emphasizeCvsWorker.ts
+++ AARC-Frontend/src/models/cvs/workers/emphasizeCvsWorker.ts
@@ -9,25 +9,28 @@ export const useEmphasizeCvsWorker = defineStore('emphasizeCvsWorker', ()=>{
     const saveStore = useSaveStore()
     const cs = useConfigStore()
     function renderEmphasizesForRingLines(ctx:CanvasRenderingContext2D, range:Line[]){
-        const coords:Coord[] = []
+        const targets:{color:string, pos:Coord}[] = []
         saveStore.save?.lines.forEach(line=>{
             if(!range.some(r=>r.id===line.id))
                 return
             if(isRing(line)){
                 const pt = saveStore.getPtById(line.pts[0])
                 if(pt)
-                    coords.push(pt?.pos)
+                    targets.push({pos:pt.pos, color:line.color})
             }
         })
         const radius = cs.config.ptStaSize*2
-        coords.forEach(coord=>{
-            renderEmphasizeSingle(ctx, coord, radius)
+        targets.forEach(t=>{
+            renderEmphasizeSingle(ctx, t.pos, t.color, radius)
         })
     }
-    function renderEmphasizeSingle(ctx:CanvasRenderingContext2D, coord:Coord, radius:number){
+    function renderEmphasizeSingle(ctx:CanvasRenderingContext2D, coord:Coord, color:string, radius:number){
         ctx.beginPath()
         ctx.arc(...coord, radius, 0, 2*Math.PI)
-        ctx.strokeStyle = 'red'
+        ctx.strokeStyle = cs.config.bgColor
+        ctx.lineWidth = 10
+        ctx.stroke()
+        ctx.strokeStyle = color
         ctx.lineWidth = 5
         ctx.stroke()
     }
