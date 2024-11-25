b80e41bed7c228c1f3017ceb0d91f885a52cd264
Au <18971501210@189.cn>
2024-11-25 20:01:09 +0800

    修复顺序检查函数的问题

--- AARC-Frontend/src/utils/lang/checkOrder.ts
+++ AARC-Frontend/src/utils/lang/checkOrder.ts
@@ -3,11 +3,10 @@ export function checkOrder<T>(array:Array<T>, order:'asc'|'desc', orderBy:(item:
         return true
     const check:(a:number,b:number)=>boolean
          = order=='asc' ? (a,b) => b>=a : (a,b) => b<=a
-    for(let i=0; i<array.length-2; i++){
-        const a = array[i]
-        const b = array[i+1]
-        const aBy = orderBy(a)
-        const bBy = orderBy(b)
-        return check(aBy, bBy)
+    const orderBys = array.map(x=>orderBy(x))
+    for(let i=0; i<=array.length-2; i++){
+        const a = orderBys[i]
+        const b = orderBys[i+1]
+        return check(a, b)
     }
 }
