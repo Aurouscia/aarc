90687e6f5175e9581948b3fdceb4db89347e0042
Au <18971501210@189.cn>
2024-11-25 20:25:21 +0800

    地形颜色预设选择功能

--- AARC-Frontend/src/components/sideList/Terrains.vue
+++ AARC-Frontend/src/components/sideList/Terrains.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SideBar from '../common/SideBar.vue';
 import { onMounted, onUnmounted, ref } from 'vue';
-import { LineType } from '@/models/save';
+import { ColorPreset, Line, LineType } from '@/models/save';
 import { useConfigStore } from '@/models/stores/configStore';
 import { useSideListShared } from './shared/sideListShared';
 
@@ -13,12 +13,12 @@ const {
 
 const cs = useConfigStore()
 
-const editingColorLineId = ref<number>()
-function toggleEditingColorLineId(lineId?:number){
-    if(lineId && lineId == editingColorLineId.value)
-        editingColorLineId.value = undefined
+const editingColorLine = ref<Line>()
+function toggleEditingColorLineId(line?:Line){
+    if(line===editingColorLine.value && !!line)
+        editingColorLine.value = undefined
     else
-        editingColorLineId.value = lineId
+        editingColorLine.value = line
 }
 
 defineExpose({
@@ -35,30 +35,33 @@ onUnmounted(()=>{
 
 <template>
     <SideBar ref="sidebar" :shrink-way="'v-show'" class="arrangeableList"
-        @extend="registerLinesArrange" @fold="disposeLinesArrange">
+        @extend="registerLinesArrange" @fold="disposeLinesArrange" @click="toggleEditingColorLineId()">
         <div class="lines" :class="{arranging: arrangingId >= 0}">
-            <div v-for="l in terrains" :key="l.id" :class="{arranging: arrangingId==l.id}" @click="toggleEditingColorLineId()">
+            <div v-for="l in terrains" :key="l.id" :class="{arranging: arrangingId==l.id}">
                 <div class="sqrBtn" :style="{backgroundColor: l.color}"
-                    @click="e=>{toggleEditingColorLineId(l.id);e.stopPropagation()}"></div>
-                <div v-if="editingColorLineId===l.id" class="colorPanel" @click="e=>e.stopPropagation()">
+                    @click="e=>{toggleEditingColorLineId(l);e.stopPropagation()}"></div>
+                <div v-if="editingColorLine===l" class="colorPanel" @click="e=>e.stopPropagation()">
                     <div>
-                        <input v-model="l.color" type="color" @blur="envStore.lineColorChanged"/>
-                        <input name="colorType" type="radio" id="colorCustom"/>
+                        <div class="colorInputConatainer">
+                            <label for="customColorInput" :style="{backgroundColor:l.color}">　</label>
+                            <input v-model="l.color" id="customColorInput" type="color" @blur="envStore.lineColorChanged"/>
+                        </div>
+                        <input name="colorType" type="radio" id="colorCustom" :value="undefined" v-model="l.colorPre"/>
                         <label for="colorCustom">自定义</label>
                     </div>
                     <div>
                         <div class="preset" :style="{backgroundColor: cs.config.colorPresetWater}"></div>
-                        <input name="colorType" type="radio" id="colorWater"/>
+                        <input name="colorType" type="radio" id="colorWater" :value="ColorPreset.water" v-model="l.colorPre"/>
                         <label for="colorWater">水系</label>
                     </div>
                     <div>
                         <div class="preset" :style="{backgroundColor: cs.config.colorPresetGreenland}"></div>
-                        <input name="colorType" type="radio" id="colorGreenland"/>
+                        <input name="colorType" type="radio" id="colorGreenland" :value="ColorPreset.greenland" v-model="l.colorPre"/>
                         <label for="colorGreenland">绿地</label>
                     </div>
                     <div>
                         <div class="preset" :style="{backgroundColor: cs.config.colorPresetArea}"></div>
-                        <input name="colorType" type="radio" id="colorArea"/>
+                        <input name="colorType" type="radio" id="colorArea" :value="ColorPreset.area" v-model="l.colorPre"/>
                         <label for="colorArea">区域</label>
                     </div>
                 </div>
@@ -82,6 +85,7 @@ onUnmounted(()=>{
 
 <style scoped lang="scss">
 .lines>div{
+    position: relative;
     .colorPanel{
         position: absolute;
         z-index: 10;
@@ -92,26 +96,29 @@ onUnmounted(()=>{
         background-color: inherit;
         box-shadow: 0px 0px 5px 2px black;
         display: flex;
-        gap: 3px;
+        gap: 5px;
         flex-direction: column;
-        input[type=color]{
-            width: 25px;
-            height: 25px;
-            padding: 2px 0px 2px 0px;
-            box-sizing: border-box;
-        }
-        input, label{
-            cursor: pointer;
-        }
         .preset{
             width: 25px;
             height: 25px;
             border-radius: 1000px;
         }
+        .colorInputConatainer{
+            label{
+                width: 25px;
+                height: 25px;
+                border-radius: 5px;
+            }
+        }
     }
     .colorPanel>div{
         display: flex;
-        gap: 2px
+        gap: 3px
+    }
+    .colorPanel>div:first-child{
+        border-bottom: 1px solid #ccc;
+        padding-bottom: 4px;
+        margin-bottom: -2px;
     }
 }
 </style>
