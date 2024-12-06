161b8fff2060867eca1b034aa1f682b65056a1f8
Au <18971501210@189.cn>
2024-12-06 20:42:08 +0800

    添加vite-plugin-checker

--- AARC-Frontend/vite.config.ts
+++ AARC-Frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({vueTsc:true})
+  ],
   server:{
     port:5173
   },
