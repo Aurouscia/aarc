c34f41e3f64ae9bd7d31eaef28ece8af8ad64b10
Au <18971501210@189.cn>
2024-12-28 23:32:32 +0800

    【编辑器】修复画布上下文过期的问题

--- AARC-Frontend/src/models/cvs/common/cvs.ts
+++ AARC-Frontend/src/models/cvs/common/cvs.ts
@@ -1,15 +1,17 @@
-import { ref, shallowRef } from "vue";
+import { ref } from "vue";
 
 export function useCvs(){
     const cvs = ref<HTMLCanvasElement>()
-    const ctx = shallowRef<CanvasRenderingContext2D>()
+    let ctx:CanvasRenderingContext2D|undefined = undefined;
+    let ctxTakenFrom:HTMLCanvasElement|undefined = undefined;
     function getCtx(){
-        if(ctx.value)
-            return ctx.value;
+        if(ctx && ctxTakenFrom === cvs.value)
+            return ctx;
         const res = cvs.value?.getContext('2d')
         if(!res)
             throw Error('获取画布上下文失败')
-        ctx.value = res;
+        ctxTakenFrom = cvs.value
+        ctx = res;
         return res;
     }
     function getCtxWithClearing(clear = true){
