d6460e62c613c1b773c449f8db3fb45adf9fa409
Au <18971501210@189.cn>
2025-01-01 02:22:57 +0800

    【前端】demo入口

--- AARC-Frontend/src/pages/editors/Editor.vue
+++ AARC-Frontend/src/pages/editors/Editor.vue
@@ -5,7 +5,7 @@ import UnsavedLeavingWarning from '@/components/common/UnsavedLeavingWarning.vue
 import { useSaveStore } from '@/models/stores/saveStore';
 import { useUniqueComponentsStore } from '@/app/globalStores/uniqueComponents';
 import { storeToRefs } from 'pinia';
-import { onBeforeMount, onUnmounted, ref } from 'vue';
+import { computed, onBeforeMount, onUnmounted, ref } from 'vue';
 import { devSave } from '@/dev/devSave';
 import { useApiStore } from '@/app/com/api';
 import { ensureValidSave } from '@/models/save';
@@ -19,6 +19,7 @@ const api = useApiStore().get()
 const saveIdNum = parseInt(props.saveId)
 const loadComplete = ref(false)
 const mainCvsDispatcher = useMainCvsDispatcher()
+const isDemo = computed(()=>props.saveId.toLowerCase() == 'demo')
 async function load() {
     if(!isNaN(saveIdNum)){
         const resp = await api.save.loadData(saveIdNum)
@@ -30,14 +31,22 @@ async function load() {
             pop.value?.show('存档损坏，请联系管理员', 'failed')
         }
     }
-    else if(props.saveId.toLowerCase() == 'demo'){
-        saveStore.save = devSave
+    else if(isDemo.value){
+        saveStore.save = JSON.parse(JSON.stringify(devSave))
         loadComplete.value = true
+        pop.value?.show('此处为体验环境，不能保存', 'warning')
+        window.setTimeout(()=>{
+            pop.value?.show('如需创作，请注册账户并新建存档', 'warning')
+        }, 3000)
     }
 }
 
 const { preventLeaving, releasePreventLeaving, showUnsavedWarning } = usePreventLeavingUnsaved()
 async function saveData(){
+    if(isNaN(saveIdNum)){
+        pop.value?.show('此处不能保存', 'failed')
+        return
+    }
     const data = JSON.stringify(saveStore.save)
     const resp = await api.save.updateData(saveIdNum, data)
     if(resp){
@@ -47,7 +56,8 @@ async function saveData(){
 
 onBeforeMount(async()=>{
     //将“主画布重新渲染”当成“存档信息变化”，当主画布重新渲染时，阻止用户离开/刷新页面/关闭页面
-    mainCvsDispatcher.afterMainCvsRendered = preventLeaving
+    if(!isDemo.value)
+        mainCvsDispatcher.afterMainCvsRendered = preventLeaving
     topbarShow.value = false
     await load()
 })
