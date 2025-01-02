9710ee6f85663eb10468517aad7c6dec4a71d9ef
Au <18971501210@189.cn>
2025-01-03 00:34:19 +0800

    【编辑器】本地设置页显示浏览器信息

--- AARC-Frontend/src/components/LocalConfigs.vue
+++ AARC-Frontend/src/components/LocalConfigs.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue';
 import SideBar from './common/SideBar.vue';
 import { useScalerLocalConfigStore } from '@/app/localConfig/scalerLocalConfig';
+import Bowser from 'bowser'
 
 const steppedScaleEnabled = ref(false)
 const scalerLocalConfig = useScalerLocalConfigStore()
@@ -16,8 +17,10 @@ defineExpose({
     fold: ()=>{sidebar.value?.fold()}
 })
 
+const browserInfo = ref<ReturnType<typeof Bowser.parse>>()
 onMounted(()=>{
     steppedScaleEnabled.value = scalerLocalConfig.readSteppedScaleEnabled()
+    browserInfo.value = Bowser.parse(navigator.userAgent)
 })
 </script>
 
@@ -32,10 +35,20 @@ onMounted(()=>{
             步进式缩放(仅限触屏)
         </td>
     </tr>
+    <tr v-if="browserInfo">
+        <td colspan="2" class="browserInfo">
+            <p>软件：{{ browserInfo.browser.name }}</p>
+            <p>引擎：{{ browserInfo.engine.name }}</p>
+            <p>平台：{{ browserInfo.platform.type }}</p>
+            <p>系统：{{ browserInfo.os.name }}</p>
+        </td>
+    </tr>
 </tbody></table>
 </SideBar>
 </template>
 
 <style scoped lang="scss">
-
+.browserInfo{
+    text-align: left;
+}
 </style>
