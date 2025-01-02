9d4b6902cbe1783e69667e4383380a90a840e575
Au <18971501210@189.cn>
2025-01-02 23:02:27 +0800

    【编辑器】主画布渲染计时

--- AARC-Frontend/src/models/cvs/dispatchers/mainCvsDispatcher.ts
+++ AARC-Frontend/src/models/cvs/dispatchers/mainCvsDispatcher.ts
@@ -8,8 +8,9 @@ import { defineStore } from "pinia";
 import { useTerrainSmoothCvsWorker } from "../workers/terrainSmoothCvsWorker";
 import { LineType } from "@/models/save";
 import { useTextTagCvsWorker } from "../workers/textTagCvsWorker";
-import { shallowRef } from "vue";
+import { ref, shallowRef } from "vue";
 import { useConfigStore } from "@/models/stores/configStore";
+import { timestampMS } from "@/utils/timeUtils/timestamp";
 
 export interface MainCvsRenderingOptions{
     /** 这次渲染前哪些线路形状变动了？不提供即为所有 */
@@ -33,8 +34,11 @@ export const useMainCvsDispatcher = defineStore('mainCvsDispatcher', ()=>{
     const { renderAllTerrainSmooth } = useTerrainSmoothCvsWorker()
     const { renderAllTextTags } = useTextTagCvsWorker()
     const afterMainCvsRendered = shallowRef<()=>void>()
+    const isRendering = ref(false)
+    const logRendering = import.meta.env.VITE_LogMainCvsRendering === 'true'
     function renderMainCvs(options:MainCvsRenderingOptions){
-        console.log('绘制主画布')
+        const tStart = logRendering ? timestampMS():0
+        isRendering.value = true
         const ctx = getCtx();
         const { changedLines, movedStaNames, suppressRenderedCallback, forExport } = options
         if(forExport){
@@ -49,13 +53,24 @@ export const useMainCvsDispatcher = defineStore('mainCvsDispatcher', ()=>{
         renderClusters(ctx)
         renderAllPtName(ctx, movedStaNames)
         renderAllTextTags(ctx)
+        isRendering.value = false
+        if(logRendering){
+            const tEnd = logRendering ? timestampMS():0
+            console.log(`主画布渲染 [${tEnd - tStart}ms]`)
+        }
         if(!suppressRenderedCallback && afterMainCvsRendered.value)
             afterMainCvsRendered.value()
     }
-    envStore.rerender = (changedLines, movedStaNames)=>
+    envStore.rerender = (changedLines, movedStaNames)=>{
         renderMainCvs({
             changedLines,
             movedStaNames
         })
+    }
+    // envStore.rescaled.push(()=>renderMainCvs({
+    //     changedLines:[],
+    //     movedStaNames:[],
+    //     suppressRenderedCallback:true
+    // }))
     return { mainCvs, renderMainCvs, afterMainCvsRendered }
 })
