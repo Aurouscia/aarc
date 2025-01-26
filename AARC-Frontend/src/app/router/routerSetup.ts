6e3e0113008a443355284f2985ec4264871e1251
Au <18971501210@189.cn>
2025-01-26 14:27:45 +0800

    【前端】离开编辑器后自动刷新一次

--- AARC-Frontend/src/app/router/routerSetup.ts
+++ AARC-Frontend/src/app/router/routerSetup.ts
@@ -1,9 +1,10 @@
-import { Router } from "vue-router"
+import { isNavigationFailure, Router } from "vue-router"
 import NotFoundPage from "@/pages/NotFoundPage.vue"
 import { addIdentitiesPages } from "@/pages/identities/routes/routesInit"
 import { addSavesPages } from "@/pages/saves/routes/routesInit"
 import { addEditorsPages } from "@/pages/editors/routes/routesInit"
 import { addHomesPages } from "@/pages/homes/routes/routesInit"
+import { editorName } from "@/pages/editors/routes/routesNames"
 
 export function routerSetup(router:Router){
     router.addRoute({
@@ -14,4 +15,11 @@ export function routerSetup(router:Router){
     addIdentitiesPages(router)
     addSavesPages(router)
     addEditorsPages(router)
+    router.afterEach((to, from, failure)=>{
+        if(isNavigationFailure(failure))
+            return
+        if(to.name!==editorName && from.name===editorName){
+            window.location.reload()
+        }
+    })
 }
