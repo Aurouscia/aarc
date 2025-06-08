9076525145f3fb75e8eeb998d947e5a7a4f7743a
Au <18971501210@189.cn>
2025-06-09 00:28:36 +0800

    【前端】改动特定源码时，HMR改为刷新页面

--- AARC-Frontend/vite.config.ts
+++ AARC-Frontend/vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { appVersionMark } from '@aurouscia/vite-app-version'
+import fullReload from 'vite-plugin-full-reload'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    appVersionMark()
+    appVersionMark(),
+    fullReload([
+      "src/app/**", "src/models/**", "src/utils/**"
+    ])
   ],
   server:{
     port:5173
