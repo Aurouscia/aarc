e33fe6ca6fa792ec10a2364f56764ee78236d959
Au <18971501210@189.cn>
2025-07-03 14:19:54 +0800

    【项目】git commit-msg钩子脚本及其用法

---
+++ commit-msg.mjs
@@ -0,0 +1,21 @@
+// ==========================================
+// 提交信息钩子（确保提交信息符合约定）
+// 使用方法：
+// 1. 在 .git/hooks 目录下创建 commit-msg 文件（不要后缀名）
+// 2. 在 pre-commit 文件中添加以下内容：（即使在windows系统中，第一行的shebang也必须有）
+
+// #!/bin/sh
+// exit $(node ./commit-msg.mjs $1)
+// ==========================================
+
+import { readFileSync } from 'fs';
+
+const commitMsgFilePath = process.argv[2]
+const commitMsg = readFileSync(commitMsgFilePath, 'utf-8');
+const requiredKeywords = ['【后端】','【前端】','【前端/后端】','【编辑器】','【项目】'];
+const hasKeyword = requiredKeywords.some(keyword => commitMsg.startsWith(keyword));
+
+if (!hasKeyword) {
+    console.error(`提交信息必须以以下关键字开头：`+ requiredKeywords.join('/'));
+    process.exit(1); // 退出代码 1 表示失败
+}
