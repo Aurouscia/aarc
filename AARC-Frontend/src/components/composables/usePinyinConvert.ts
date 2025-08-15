1fe3111a2c029629567656d3a8faba4bdfc81368
Au <18971501210@189.cn>
2025-08-15 16:34:26 +0800

    【编辑器】“拼音转换”按钮：避免二次点击重复发送http请求

--- AARC-Frontend/src/components/composables/usePinyinConvert.ts
+++ AARC-Frontend/src/components/composables/usePinyinConvert.ts
@@ -5,13 +5,27 @@ type TargetRef = Ref<string|undefined|null>
 export const usePinyinConvert = (source:TargetRef, dest:TargetRef, afterDone:()=>void)=>{
     const api = useApiStore()
     const overridingForSource = ref<string>()
+    const overridingToDest = ref<string>()
     const overridingReady = computed<boolean>(()=>overridingForSource.value === source.value)
     async function convertPinyin(){
         if(!source.value)
             return
-        const res = await api.saveUtils.pinyinConvert({
-            text: source.value
-        })
+        let res:string|undefined|null
+        if(overridingReady.value && overridingToDest.value){
+            //若现在已经是二次点击，则无需重新用http获取
+            res = overridingToDest.value
+        }
+        else{
+            res = await api.saveUtils.pinyinConvert({
+                text: source.value
+            })
+        }
+        const apply = ()=>{
+            overridingForSource.value = undefined
+            overridingToDest.value = undefined
+            dest.value = res
+            afterDone()
+        }
         if(res){
             if(dest.value){
                 //目标Ref已经有值，判断是否覆盖
@@ -21,17 +35,14 @@ export const usePinyinConvert = (source:TargetRef, dest:TargetRef, afterDone:()=
                 if(!overridingReady.value){
                     //“确认”值和源不同：把“确认”值设为源（要求二次点击）
                     overridingForSource.value = source.value
+                    overridingToDest.value = res //记录结果，可避免重复获取
                 }else{
                     //“确认”值和源相同：应用
-                    overridingForSource.value = undefined
-                    dest.value = res
-                    afterDone()
+                    apply()
                 }
             }else{
                 //目标Ref没值，应用
-                overridingForSource.value = undefined
-                dest.value = res
-                afterDone()
+                apply()
             }
         }
     }
