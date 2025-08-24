412e926928763947839c09d67eaf504e5a71fe58
Au <18971501210@189.cn>
2025-08-24 21:39:37 +0800

    【编辑器】调色盘内置色库：汇总文件及其生成脚本

---
+++ AARC-Frontend/src/data/palette/paletteGen.ts
@@ -0,0 +1,61 @@
+import fs from 'fs'
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
+const res:string[] = []
+const resPath = join(__dirname, 'palette.ts')
+
+res.push('//=======================================================')
+res.push('//本文件为自动生成，请勿手动编辑，操作方法见同目录的README.md')
+res.push('//=======================================================')
+res.push('')
+writeForSubDir('real')
+writeForSubDir('fic')
+fs.writeFileSync(resPath, res.join('\n'))
+
+function writeForSubDir(subDirNamePure:string){
+    const subDir = join(__dirname, subDirNamePure)
+    if(!fs.existsSync(subDir))
+        return
+    // 遍历子目录下的所有文件
+    const files = fs.readdirSync(subDir)
+    const citiesHere:CityTemp[] = []
+    for (const file of files) {
+        if(!file.endsWith('.ts'))
+            continue
+        const priStr = file.split('-', 1).at(0)
+        const restEnd = file.length - 3
+        const rest = file.substring(4, restEnd)
+        if(!priStr || !rest)
+            continue
+        const pri = parseInt(priStr)
+        if(isNaN(pri))
+            continue
+        citiesHere.push({
+            pri,
+            dirName: subDirNamePure,
+            displayName: rest,
+            fileName: file,
+            importName: `${subDirNamePure}_${pri}`
+        })
+    }
+    citiesHere.sort((x,y)=> x.pri-y.pri)
+    for (const city of citiesHere){
+        res.push(`import ${city.importName} from './${subDirNamePure}/${city.fileName}'`)
+    }
+    res.push(`export const ${subDirNamePure} = [`)
+    for(const c of citiesHere)
+        res.push(`    {name:'${c.displayName}', pri:${c.pri}, data:${c.importName}},`)
+    res.push(']')
+    res.push('')
+}
+
+interface CityTemp{
+    pri:number,
+    dirName:string
+    displayName:string,
+    fileName:string,
+    importName:string
+}
---
+++ AARC-Frontend/src/data/palette/real/998-示例城市-示例公司.ts
@@ -0,0 +1,4 @@
+export default `
+1号线: #ff0000
+2号线: #ff00ff
+`
