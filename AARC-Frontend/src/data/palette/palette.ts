36423e9c8a4cf35e02a7a1d294694a36abdeb1b1
binshu2233 <binshu2233@user.noreply.gitee.com>
2025-08-25 23:10:38 +0800

    【编辑器】颜色库数据 深圳 南宁

--- AARC-Frontend/src/data/palette/palette.ts
+++ AARC-Frontend/src/data/palette/palette.ts
@@ -6,12 +6,16 @@ import {ref} from 'vue';
 import real_0 from './real/000-北京.ts'
 import real_1 from './real/001-上海.ts'
 import real_2 from './real/002-广州.ts'
+import real_3 from './real/003-深圳.ts'
 import real_4 from './real/004-武汉.ts'
+import real_20 from './real/020-南宁.ts'
 export const real = [
     {name:'北京', pri:0, data:real_0,open:ref(false)},
     {name:'上海', pri:1, data:real_1,open:ref(false)},
     {name:'广州', pri:2, data:real_2,open:ref(false)},
+    {name:'深圳', pri:3, data:real_3,open:ref(false)},
     {name:'武汉', pri:4, data:real_4,open:ref(false)},
+    {name:'南宁', pri:20, data:real_20,open:ref(false)},
 ]
 
 import fic_999 from './fic/999-示例作品.ts'
