4cabd958026a6fd49e0550abd85e48a64a51dffe
Au <18971501210@189.cn>
2025-11-02 00:16:15 +0800

    【前端】优化git信息获取和展示

--- AARC-Frontend/build.ts
+++ AARC-Frontend/build.ts
@@ -4,6 +4,7 @@ const firstParam = process.argv[2]
 const buildHere = firstParam?.toLowerCase() === '--here'
 const viteBuildArgs = buildHere ? ['--outDir', './dist'] : []
 
+const gitInfoGen = execa('npx', ['tsx', './src/utils/gitInfo/gitInfoGen.ts', '--silence'], {stdout:'inherit', reject:true})
 const viteBuild = execa("npx", ["vite", "build", ...viteBuildArgs], {stdout:'inherit', reject:true});
 const vueTsc = execa("npx", ["vue-tsc"], {stdout:'inherit', reject:true})
-await Promise.all([vueTsc, viteBuild])
+await Promise.all([gitInfoGen, vueTsc, viteBuild])
