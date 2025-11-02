e757137a891e2063bc16dcfbb0ade18d34b038cb
Au <18971501210@189.cn>
2025-11-02 23:20:29 +0800

    【编辑器】更新颜色选择器，改为使用v-model绑定颜色值

--- AARC-Frontend/src/components/sidebars/shared/ColorPickerForTerrain.vue
+++ AARC-Frontend/src/components/sidebars/shared/ColorPickerForTerrain.vue
@@ -2,34 +2,36 @@
 import { Line } from '@/models/save';
 import { useEnvStore } from '@/models/stores/envStore';
 import { AuColorPickerPresetsNested } from '@aurouscia/au-color-picker';
-import { CSSProperties, ref } from 'vue';
+import { CSSProperties, onBeforeMount, ref, watch } from 'vue';
 import { useColorPresetNames } from '../sideList/shared/useColorPresetNames';
 
 const envStore = useEnvStore()
 const picker = ref<InstanceType<typeof AuColorPickerPresetsNested>>()
-
-const { getPresetNameByEnum, getPresetEnumByName, presets } = useColorPresetNames()
-
-function colorPreChanged(presetName:string|undefined){
-    props.line.colorPre = getPresetEnumByName(presetName)
-    emit('colorUpdated')
-    window.setTimeout(()=>{
-        envStore.lineInfoChanged(props.line)
-    },1)
-}
-function colorPickerDone(c:string){
-    props.line.color = c
-    if(!props.line.colorPre){
-        envStore.lineInfoChanged(props.line)
-        emit('colorUpdated')
-    }
-}
-
 const props = defineProps<{
     line:Line,
     zIndex?:number,
     entryStyles?:CSSProperties
 }>()
+
+const { getPresetNameByEnum, getPresetEnumByName, presets } = useColorPresetNames()
+
+const colorPreNameSelected = ref<string>()
+onBeforeMount(()=>{
+    colorPreNameSelected.value = getPresetNameByEnum(props.line.colorPre)
+})
+watch(()=>colorPreNameSelected.value, (presetName:string|undefined)=>{
+    props.line.colorPre = getPresetEnumByName(presetName)
+    emit('colorUpdated')
+    window.setTimeout(()=>{
+        envStore.lineInfoChanged(props.line)
+    },1)
+})
+watch(()=>props.line.color, ()=>{
+    if(!props.line.colorPre){
+        envStore.lineInfoChanged(props.line)
+        emit('colorUpdated')
+    }
+})
 defineExpose({
     close:()=>picker.value?.closePanel()
 })
@@ -40,11 +42,10 @@ const emit = defineEmits<{
 
 <template>
     <AuColorPickerPresetsNested
-        :initial="line.color"
+        v-model="line.color"
+        v-model:model-value-selected-preset="colorPreNameSelected"
         :presets="presets"
         :initial-selected-preset="getPresetNameByEnum(line.colorPre)"
-        @preset-switched="n=>colorPreChanged(n)"
-        @done="c=>colorPickerDone(c)"
         :show-package-name="true"
         :entry-respond-delay="1"
         ref="picker" :panel-base-z-index="zIndex"
