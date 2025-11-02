0fb2eee2ff5201389d0c12eea12cb807ea77081a
binshu2233 <binshu2233>
2025-11-02 19:27:37 +0800

    【编辑器】导出线路列表

--- AARC-Frontend/src/components/sidebars/configs/ExportInfo.vue
+++ AARC-Frontend/src/components/sidebars/configs/ExportInfo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref} from 'vue';
+import { ref } from 'vue';
 import SideBar from '@/components/common/SideBar.vue';
 import { useStaClusterStore } from '@/models/stores/saveDerived/staClusterStore';
 import { useSaveStore } from '@/models/stores/saveStore';
@@ -14,42 +14,86 @@ const saveStore = useSaveStore()
 const staClusterStore = useStaClusterStore()
 const { pop } = useUniqueComponentsStore()
 
+const wikiMode=ref(true)
+
 const staNameSplitChar = ref('')
-const autoLineSuffix = ref(false)
+const autoLineSuffix = ref(true)
 const autoLineSuffixHaoxian = ref(false)
-const removeZeroOnFirstChar = ref(false)
+const removeZeroOnFirstChar = ref(true)
 const exportColorInfo = ref(true)
 
-async function downloadStaNameListTxt() {
+function parseLineName(lname: string) {
+    if (removeZeroOnFirstChar.value) {
+        lname = lname.replace(/^0+/, '')
+    }
+    if (autoLineSuffix.value) {
+        if (/^\d+$/.test(lname)) {
+            //1号线
+            lname += '号线'
+        }
+        if (/^[a-zA-Z]+$/.test(lname)) {
+            //A线
+            lname += '线';
+        }
+        // 检查是否是字母和数字的组合（至少包含一个字母）
+        if (/^(?=.*[a-zA-Z])[a-zA-Z0-9]+$/.test(lname)) {
+            if (autoLineSuffixHaoxian.value) {
+                //S1号线
+                lname += '号线'
+            }
+            else {
+                //S1线
+                lname += '线';
+            }
+        }
+    }
+    return lname
+}
+function getStaName(ptid: number) {
+    const clusters = staClusterStore.getStaClusters()
+    const cluster = clusters?.find(cluster =>
+        cluster.some(sta => sta.id === ptid)
+    );
+    if (!cluster) {
+        let point = saveStore.save?.points.find(x => x.id == ptid)
+        if (point)
+            return point.name
+        else
+            return ''
+    }
+    else {
+        let clusterHaveName = cluster.find(x => x.name)
+        if (clusterHaveName)
+            return clusterHaveName.name
+        else
+            return ''
+    }
+}
+async function copyLineListTxt() {
+    let txt = '|线路|颜色|起点|终点|\n'
+    saveStore.save?.lines.filter(l => l.type == LineType.common && !l.isFake).forEach(l => {
+        let lname = parseLineName(l.name)
+        txt += `|${lname}|/-c-/${l.color}|`
+        let firstStaName = getStaName(l.pts[0])
+        let lastStaName = getStaName(l.pts[l.pts.length - 1])
+        if (firstStaName != lastStaName)
+            txt += `${firstStaName}|${lastStaName}|`
+        else {
+            txt += `环行||`
+        }
+        txt+='\n'
+    })
+    copyText(txt)
+}
+async function copyStaNameListTxt() {
     let txt = ''
     const clusters = staClusterStore.getStaClusters()
     saveStore.save?.lines.filter(l => l.type == LineType.common && !l.isFake).forEach(l => {
-        let lname = l.name
-        if (removeZeroOnFirstChar.value) {
-            lname = lname.replace(/^0+/, '')
+        let lname = parseLineName(l.name)
+
+        if (exportColorInfo.value) {
+            txt += `# ${lname} \n${l.color}#\n`
         }
-        if (autoLineSuffix.value) {
-            if (/^\d+$/.test(lname)) {
-                //1号线
-                lname += '号线'
-            }
-            if (/^[a-zA-Z]+$/.test(lname)) {
-                //A线
-                lname += '线';
-            }
-            // 检查是否是字母和数字的组合（至少包含一个字母）
-            if (/^(?=.*[a-zA-Z])[a-zA-Z0-9]+$/.test(lname)) {
-                if (autoLineSuffixHaoxian.value) {
-                    //S1号线
-                    lname += '号线'
-                }
-                else {
-                    //S1线
-                    lname += '线';
-                }
-            }
-        }
-        if (exportColorInfo.value) { txt += `# ${lname}\n${l.color}#\n` }
         else {
             txt += `# ${lname}\n`
         }
@@ -80,14 +124,16 @@ async function downloadStaNameListTxt() {
             txt += stationNameList.join(splitChar) + '\n'
         }
     })
+    copyText(txt)
+}
+function copyText(txt: string) {
     const success = copy(txt);
     if (success) {
-        pop?.show('已复制', 'success');
+        pop?.show(`复制成功，共${txt.length}字`, 'success');
     } else {
         pop?.show('复制失败，请改用正规浏览器', 'failed');
     }
 }
-
 defineExpose({
     comeOut: () => { sidebar.value?.extend() },
     fold: () => { sidebar.value?.fold() }
@@ -99,6 +145,17 @@ defineExpose({
     <ConfigSection :title="'导出作品信息'">
         <table>
             <tbody>
+                <tr>
+                    <td>
+                        站名分隔符
+                        <div class="note">
+                            默认为空格；用\n换行
+                        </div>
+                    </td>
+                    <td>
+                        <input v-model="staNameSplitChar" style="width: 2em;">
+                    </td>
+                </tr>
                 <tr>
                     <td>
                         站名分隔符
@@ -153,53 +210,20 @@ defineExpose({
                 </tr>
             </tbody>
         </table>
-        <button @click="downloadStaNameListTxt" class="minor">复制站名列表（适用于wiki）</button>
+        <button @click="copyStaNameListTxt" class="minor">复制站名列表（适用于Fic3Wiki）</button>
+        <button @click="copyLineListTxt" class="minor">复制线路列表（适用于Fic3Wiki）</button>
     </ConfigSection>
 </template>
 
 <style scoped lang="scss">
-.ok {
-    margin-top: 20px;
-}
-
-.configItem {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    .itemName {
-        color: #666;
-    }
-
-    input {
-        max-width: 120px;
-    }
-}
-
-.explainItem {
-    color: #333;
-    font-size: 14px;
-}
-
-.exportOps {
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-}
 
 .note {
     margin: 8px 0px;
     font-size: 14px;
     color: #999;
     text-align: center;
-
-    .downloadAnchor {
-        color: cornflowerblue;
-        text-decoration: underline;
-    }
 }
-
-.browserLimit {
-    font-size: 14px;
+button{
+    text-align: center;
 }
 </style>
