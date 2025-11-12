4fbae0c004d2999ef313467aa695831db2ca358b
Au <18971501210@189.cn>
2025-11-12 22:41:11 +0800

    【前端】版本检查后，强制刷新页面

--- AARC-Frontend/src/app/appVersionCheck.ts
+++ AARC-Frontend/src/app/appVersionCheck.ts
@@ -5,6 +5,9 @@ export async function appVersionCheck(){
     const { pop } = useUniqueComponentsStore()
     const res = await check()
     if(!res){
-        pop?.show('有版本更新，请刷新浏览器', 'warning')
+        pop?.show('版本更新\n为您刷新浏览器', 'warning')
+        window.setTimeout(()=>{
+            window.location.reload()
+        }, 1000)
     }
 }
