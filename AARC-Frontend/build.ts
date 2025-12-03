c288b6492dbf469064be5108f972418851ba26ba
Au <18971501210@189.cn>
2025-12-03 20:36:21 +0800

    build: 前端编译时将apiGenerated.ts复制到public目录

--- AARC-Frontend/build.ts
+++ AARC-Frontend/build.ts
@@ -1,9 +1,11 @@
 import { execa } from 'execa'
+import fs from 'fs'
 
 const firstParam = process.argv[2]
 const buildHere = firstParam?.toLowerCase() === '--here'
 const viteBuildArgs = buildHere ? ['--outDir', './dist'] : []
 
+fs.copyFileSync('./src/app/com/apiGenerated.ts', './public/apiGenerated.ts')
 const gitInfoGen = execa('npx', ['tsx', './src/utils/gitInfo/gitInfoGen.ts', '--silence'], {stdout:'inherit', reject:true})
 const viteBuild = execa("npx", ["vite", "build", ...viteBuildArgs], {stdout:'inherit', reject:true});
 const vueTsc = execa("npx", ["vue-tsc"], {stdout:'inherit', reject:true})
