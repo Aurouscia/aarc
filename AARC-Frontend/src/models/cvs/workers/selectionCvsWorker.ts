034b83c00ada032884ee2bf4cefe986f1d16d2bb
Au <18971501210@189.cn>
2026-01-17 22:20:47 +0800

    feat(editor): 减选模式涂抹区域改为粉红色

--- AARC-Frontend/src/models/cvs/workers/selectionCvsWorker.ts
+++ AARC-Frontend/src/models/cvs/workers/selectionCvsWorker.ts
@@ -10,15 +10,16 @@ export const useSelectionCvsWorker = defineStore('selectionCvsWorker', ()=>{
         const cursor = envStore.cursorPos
         if(!selStore.working || !cursor)
             return
+        const brushColor = selStore.mode == 'add' ? 'green' : 'palevioletred'
         const r = selStore.brushRadius
         ctx.beginPath()
         ctx.globalAlpha = 1
         ctx.arc(...cursor, r, 0, 2 * Math.PI)
         ctx.lineWidth = 2
-        ctx.strokeStyle = 'green'
+        ctx.strokeStyle = brushColor
         ctx.stroke()
         ctx.globalAlpha = 0.3
-        ctx.fillStyle = 'green',
+        ctx.fillStyle = brushColor,
         ctx.fill()
         ctx.globalAlpha = 1
     }
