124868a3e032e103e1260e6086e0010cf9d91e58
Au <18971501210@189.cn>
2026-01-23 15:45:08 +0800

    build: 优化前端build.ts脚本

--- AARC-Frontend/build.ts
+++ AARC-Frontend/build.ts
@@ -1,12 +1,25 @@
-import { execa } from 'execa'
+import { execa, Options as ExecaOptions } from 'execa'
 import fs from 'fs'
 
-const firstParam = process.argv[2]
-const buildHere = firstParam?.toLowerCase() === '--here'
-const viteBuildArgs = buildHere ? ['--outDir', './dist'] : []
+build()
 
-fs.copyFileSync('./src/app/com/apiGenerated.ts', './public/apiGenerated.ts')
-const gitInfoGen = execa('npx', ['tsx', './src/utils/gitInfo/gitInfoGen.ts', '--silence'], {stdout:'inherit', reject:true})
-const viteBuild = execa("npx", ["vite", "build", ...viteBuildArgs], {stdout:'inherit', reject:true});
-const vueTsc = execa("npx", ["vue-tsc"], {stdout:'inherit', reject:true})
-await Promise.all([gitInfoGen, vueTsc, viteBuild])
+async function build(){
+    const firstParam = process.argv[2]
+    const buildHere = firstParam?.toLowerCase() === '--here'
+    const checkOnly = firstParam?.toLowerCase() === '--check-only'
+    const viteBuildArgs = buildHere ? ['--outDir', './dist'] : []
+    const execaOptions: ExecaOptions = {stdout:'inherit', stderr:'inherit', reject:false}
+
+    const vueTsc = execa("npx", ["vue-tsc"], execaOptions)
+    if(checkOnly){
+        console.log('🔍 ts check started')
+        const res = await vueTsc
+        if(res.exitCode == 0) console.log('✅ ts check passed')
+        else console.log('❌ ts check failed')
+        return
+    }
+    const gitInfoGen = execa('npx', ['tsx', './src/utils/gitInfo/gitInfoGen.ts', '--silence'], {...execaOptions})
+    const viteBuild = execa("npx", ["vite", "build", ...viteBuildArgs], {...execaOptions})
+    fs.copyFileSync('./src/app/com/apiGenerated.ts', './public/apiGenerated.ts')
+    await Promise.all([gitInfoGen, vueTsc, viteBuild])
+}
