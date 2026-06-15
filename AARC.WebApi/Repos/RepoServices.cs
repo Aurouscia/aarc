04250e2f8ae0b672bab64749dc480658f3c323cc
Au <18971501210@189.cn>
2026-06-16 00:19:44 +0800

    fix: 修复UserFileRepo注册丢失的问题

--- AARC.WebApi/Repos/RepoServices.cs
+++ AARC.WebApi/Repos/RepoServices.cs
@@ -11,6 +11,7 @@ namespace AARC.WebApi.Repos
         {
             services.AddScoped<UserRepo>();
             services.AddScoped<AuthGrantRepo>();
+            services.AddScoped<UserFileRepo>();
             services.AddScoped<SaveRepo>();
             services.AddScoped<SaveFolderRepo>();
             services.AddScoped<SaveFolderRelationRepo>();
